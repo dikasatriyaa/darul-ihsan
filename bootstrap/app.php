1e2c9f468f77672968031c09ad0a840eb1e3d3ac
dikasatriyaa <dikasatriya26@gmail.com>
2026-05-17 21:47:37 +0700

    Fix Boostrap app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// Trik Laravel 12 untuk Vercel: Set folder kompilasi view secara runtime
-if (env('APP_ENV') === 'production') {
-    config(['view.compiled' => '/tmp/storage/framework/views']);
-}
-
-return Application::configure(basePath: dirname(__DIR__))
+// 1. Biarkan Laravel mengonfigurasi dirinya sendiri terlebih dahulu
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -21,3 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// 2. AMAN: Set folder kompilasi view setelah objek $app berhasil di-create
+if (env('APP_ENV') === 'production') {
+    config(['view.compiled' => '/tmp/storage/framework/views']);
+}
+
+return $app;
