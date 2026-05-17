74a91cebf1a573f6f85c60160be21e20c756fc61
dikasatriyaa <dikasatriya26@gmail.com>
2026-05-17 22:00:26 +0700

    Fix boostrap app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// 1. Biarkan Laravel mengonfigurasi dirinya sendiri terlebih dahulu
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -17,10 +16,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-// 2. AMAN: Set folder kompilasi view setelah objek $app berhasil di-create
-if (env('APP_ENV') === 'production') {
-    config(['view.compiled' => '/tmp/storage/framework/views']);
-}
-
-return $app;
