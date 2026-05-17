ef6f5aaa0ec4c06126a19818a9b99cc875c468aa
dikasatriyaa <dikasatriya26@gmail.com>
2026-05-17 22:06:28 +0700

    Fix boostrap app.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,4 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Menggunakan dirname(__DIR__) untuk memastikan posisi absolut dari root proyek Vercel
+require dirname(__DIR__) . '/public/index.php';
